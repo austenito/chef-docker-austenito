17f1613dbeaff2ac21416d22e59232f6620efab1
Austen Ito <austen.ito@gmail.com>
2015-06-07 10:55:01 -0400

    Update blog run script to use jekyll generation

--- files/default/blog/run.sh
+++ files/default/blog/run.sh
@@ -8,6 +8,8 @@ git clone https://github.com/austenito/austenito.com
 cd austenito.com
 
 export GEM_HOME=/gems/ruby/2.1.2
+export PATH=$PATH:$GEM_HOME/bin
 chruby 2.1.2
-bundle install
-RUBYOPT="-KU -E utf-8:utf-8" bundle exec rake generate
+gem install --no-document jekyll
+gem install duktape
+jekyll build
